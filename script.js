ece64424938ed944495c35a18b3f08e638c5ab07
croWbar005 <harshilsharma205@gmail.com>
2026-06-22 01:21:17 +0530

    button added to adjust number of squares in the grid.

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
-for (let x=0; x<16; x++){
-    let row = document.createElement("div");
-    row.classList.toggle("row");
-    for(let y=0; y<16; y++){
-        let t = document.createElement("div");
-        t.classList.toggle("element");
-        t.setAttribute("style","height:40px; width:40px; border:5px solid black; ");
-        t.addEventListener("mouseover", function(event){
-            t.style.backgroundColor = "green";
-        })
-        row.append(t);
+const button = document.querySelector("button");
+button.addEventListener("click", function(event){
+    container.innerHTML="";
+    let x= prompt("enter number between 1 and 100","0")
+    x=Number(x);
+    if (x === 0 || null) {
+        alert("enter a number to play");
     }
-    container.append(row);
-}
+    else if (x>100) {
+        alert("enter number below 100");
+    }
+    else {
+        let  m=1000/x;
+        for (let r=0; r<x; r++){
+        let row = document.createElement("div");
+        row.classList.toggle("row");
+        for(let y=0; y<x; y++){
+            let t = document.createElement("div");
+            t.classList.toggle("element");
+            t.setAttribute("style", `height:${m}px; width:${m}px; border:1px solid black; box-sizing: border-box;`);
+            t.addEventListener("mouseover", function(event){
+                t.style.backgroundColor = "green";
+            })
+            row.append(t);
+            }
+        container.append(row);
+        }
+    }
+})
+
