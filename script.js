df692d89690127741582f087714168779ad7bb4c
croWbar005 <harshilsharma205@gmail.com>
2026-06-22 00:14:41 +0530

    grid completed

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+for (let x=0; x<16; x++){
+    let row = document.createElement("div");
+    row.classList.toggle("row");
+    for(let y=0; y<16; y++){
+        let t = document.createElement("div");
+        t.classList.toggle("element");
+        t.setAttribute("style","height:40px; width:40px; border:5px solid black; ");
+        row.append(t);
+    }
+    container.append(row);
+}
